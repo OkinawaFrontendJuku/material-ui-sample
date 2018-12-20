1dd10c20c57aab7afd969f73b097e76324b371b6
OkinawaFrontendJuku <45957333+OkinawaFrontendJuku@users.noreply.github.com>
2018-12-20 17:41:58 +0900

    Fixed Icon Buttons.

--- src/components/templates/Header/Header.jsx
+++ src/components/templates/Header/Header.jsx
@@ -1,45 +1,42 @@
-import React from 'react';
-import AppBar from '@material-ui/core/AppBar';
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
-import Toolbar from '@material-ui/core/Toolbar';
-import IconButton from '@material-ui/core/IconButton';
-import Icon from '@material-ui/core/Icon';
-import Typography from '@material-ui/core/Typography';
-
-import { MuiThemeProvider,createMuiTheme } from '@material-ui/core/styles';
-import brown from '@material-ui/core/colors/brown';
-import red from '@material-ui/core/colors/red';
+import React from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+import Toolbar from "@material-ui/core/Toolbar";
+import IconButton from "@material-ui/core/IconButton";
+import Icon from "@material-ui/core/Icon";
+import Typography from "@material-ui/core/Typography";
+import MenuIcon from "@material-ui/icons/Menu";
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+import brown from "@material-ui/core/colors/brown";
+import red from "@material-ui/core/colors/red";
 
 const theme = createMuiTheme({
   palette: {
     primary: brown,
-    secondary: red,
-  },
+    secondary: red
+  }
 });
 
-export const Header = ({ title ,value ,open , items , callback }) =>{
-  return(
+export const Header = ({ title, value, open, items, callback }) => {
+  return (
     <AppBar position="fixed">
       <Toolbar disableGutters={!open}>
         <IconButton
           color="inherit"
           aria-label="Open drawer"
-          onClick={callback.toggleDrawer}
-        >
-          <Icon>menu</Icon>
+          onClick={callback.toggleDrawer}>
+          <MenuIcon>menu</MenuIcon>
         </IconButton>
         <Typography variant="h6" color="inherit" noWrap>
           {title}
         </Typography>
       </Toolbar>
-      <Tabs value={value} onChange={callback.selectItem} >
-        {
-          Object.keys(items).map( key =>{
-            return <Tab value={key} label={items[key].label} />
-          })
-        }
+      <Tabs value={value} onChange={callback.selectItem}>
+        {Object.keys(items).map(key => {
+          return <Tab value={key} label={items[key].label} />;
+        })}
       </Tabs>
     </AppBar>
-  )
-}
+  );
+};
