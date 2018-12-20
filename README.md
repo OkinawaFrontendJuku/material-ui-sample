329bc0e076974209c406d074d6ca4f89f7d0f729
OkinawaFrontendJuku <45957333+OkinawaFrontendJuku@users.noreply.github.com>
2018-12-20 16:53:08 +0900

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# material-ui-sample
-Created with CodeSandbox
---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+// import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+// import Home from "./components/pages/Home";
+// import Price from "./components/pages/Price";
+
+// <Route exact path="/" component={Home} />
+//   <Route exact path="/price" component={Price} />
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello World!</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
